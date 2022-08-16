59447e3692956cffec48eccf9b6ffff7740a9040
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-16 20:41:15 -0300

    Criada classe VagaController

---
+++ src/main/java/com/entra21/sistemarh/Controllers/VagaController.java
@@ -0,0 +1,16 @@
+package com.entra21.sistemarh.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+import com.entra21.sistemarh.Repositories.CandidatoRepository;
+import com.entra21.sistemarh.Repositories.VagaRepository;
+
+@Controller
+public class VagaController {
+
+    private VagaRepository vagaRepository;
+    private CandidatoRepository candidatoRepository;
+
+    
+    
+}
