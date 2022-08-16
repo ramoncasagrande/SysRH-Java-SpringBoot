f65995232bc73eeea99eb0374ae2ead5135a0235
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-16 16:34:19 -0300

    Commit Inicial

---
+++ src/main/java/com/entra21/sistemarh/SistemarhApplication.java
@@ -0,0 +1,13 @@
+package com.entra21.sistemarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemarhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entra21/sistemarh/SistemarhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21.sistemarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemarhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
