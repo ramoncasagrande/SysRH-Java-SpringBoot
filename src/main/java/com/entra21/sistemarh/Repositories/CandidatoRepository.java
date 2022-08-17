032b70fd4d6beec1803fdeb324ca5620cfea1dc1
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-16 21:28:35 -0300

    Implementada interface CandidatoRepository

--- src/main/java/com/entra21/sistemarh/Repositories/CandidatoRepository.java
+++ src/main/java/com/entra21/sistemarh/Repositories/CandidatoRepository.java
@@ -1,5 +1,11 @@
 package com.entra21.sistemarh.Repositories;
 
-public interface CandidatoRepository {
-    
+import org.springframework.data.repository.CrudRepository;
+
+import com.entra21.sistemarh.Models.Candidato;
+import com.entra21.sistemarh.Models.Vaga;
+
+public interface CandidatoRepository extends CrudRepository<Candidato, String> {
+
+    Iterable<Candidato> findByVaga(Vaga vaga);
 }
