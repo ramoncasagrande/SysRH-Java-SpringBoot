35d727a405669f7f422c17a8421bd5e15d7b1dee
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-08-16 21:04:16 -0300

    Implementada interface VagaRepository

--- src/main/java/com/entra21/sistemarh/Repositories/VagaRepository.java
+++ src/main/java/com/entra21/sistemarh/Repositories/VagaRepository.java
@@ -1,5 +1,13 @@
 package com.entra21.sistemarh.Repositories;
 
-public interface VagaRepository {
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.entra21.sistemarh.Models.Vaga;
+
+public interface VagaRepository extends CrudRepository<Vaga, String> {
+    Vaga findByCodigo(long codigo);
+    List<Vaga> findByNome(String nome);
     
 }
